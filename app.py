652fecf6374546fc7059b95181f4736c6bca7900
SeebinJwad <kazijawadahmed@gmail.com>
2022-08-06 14:55:12 -0500

    sync weblur project files

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
-@app.route('/')
-def func():
-    print(request.args())
-    return render_template('index.html')
-if __name__ == '__main__':
-    app.run(host='localhost', debug=True)
